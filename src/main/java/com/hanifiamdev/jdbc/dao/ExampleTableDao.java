0dcc96887e5ec0f8488bbb7c3e7cde10bd3fc1db
hanifiamdev <hanifamrullahdev@gmail.com>
2022-01-18 17:57:53 +0700

    JDBC Prepared Statement for insert, update and delete

--- src/main/java/com/hanifiamdev/jdbc/dao/ExampleTableDao.java
+++ src/main/java/com/hanifiamdev/jdbc/dao/ExampleTableDao.java
@@ -1,12 +1,14 @@
 package com.hanifiamdev.jdbc.dao;
 
 import com.hanifiamdev.jdbc.entity.ExampleTable;
+import lombok.extern.slf4j.Slf4j;
 
 import java.sql.*;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
+@Slf4j
 public class ExampleTableDao implements CrudRepository<ExampleTable, String> {
 
     private Connection connection;
@@ -17,23 +19,65 @@ public class ExampleTableDao implements CrudRepository<ExampleTable, String> {
 
     @Override
     public ExampleTable save(ExampleTable value) throws SQLException {
-        return null;
+        String query = "insert into example_table (id, name, created_date, created_time, is_active, counter, currency, description, floating" +
+                ")\n" +
+                "values (gen_random_uuid(), ?, ?, ?, ?, ?, ?, ?, ?)";
+        PreparedStatement preparedStatement = connection.prepareStatement(query, Statement.RETURN_GENERATED_KEYS);
+        preparedStatement.setString(1, value.getName());
+        preparedStatement.setDate(2, value.getCreatedDate());
+        preparedStatement.setTimestamp(3, value.getCreatedTime());
+        preparedStatement.setBoolean(4, value.getActive());
+        preparedStatement.setLong(5, value.getCounter());
+        preparedStatement.setBigDecimal(6, value.getCurrency());
+        preparedStatement.setString(7, value.getDescription());
+        preparedStatement.setFloat(8, value.getFloating());
+        preparedStatement.executeUpdate();
+        ResultSet generatedKeys = preparedStatement.getGeneratedKeys();
+        if (generatedKeys.next())
+            value.setId(generatedKeys.getString("id")); // bisa diisi 1, karena kolom ke - 1 untuk id
+        preparedStatement.close();
+        return value;
     }
 
     @Override
     public ExampleTable update(ExampleTable value) throws SQLException {
-        return null;
+        String query = "update example_table\n" +
+                "set name        = ?,\n" +
+                "    is_active   = ?,\n" +
+                "    counter     = ?,\n" +
+                "    currency     = ?,\n" +
+                "    description = ?,\n" +
+                "    floating    = ?\n" +
+                "where id = ?";
+        PreparedStatement preparedStatement = connection.prepareStatement(query);
+        preparedStatement.setString(1, value.getName());
+        preparedStatement.setBoolean(2, value.getActive());
+        preparedStatement.setLong(3, value.getCounter());
+        preparedStatement.setBigDecimal(4, value.getCurrency());
+        preparedStatement.setString(5, value.getDescription());
+        preparedStatement.setFloat(6, value.getFloating());
+        preparedStatement.setString(7, value.getId());
+        int row = preparedStatement.executeUpdate();
+        log.info("Success {} row Updated", row);
+        preparedStatement.close();
+        return value;
     }
 
     @Override
     public Boolean removeById(String value) throws SQLException {
-        return null;
+        String query = "delete\n" +
+                "from example_table\n" +
+                "where id = ?";
+        PreparedStatement statement = connection.prepareStatement(query);
+        statement.setString(1, value);
+        int row = statement.executeUpdate();
+        statement.close();
+        return row >= 1;
     }
 
     @Override
     public Optional<ExampleTable> findById(String id) throws SQLException {
-        //language=PostgreSQL
-        String query = "select id           as id,\n" +
+        String query = "select id    as id,\n" +
                 "       name         as name,\n" +
                 "       created_date as createdDate,\n" +
                 "       created_time as createdTime,\n" +
@@ -71,7 +115,6 @@ public class ExampleTableDao implements CrudRepository<ExampleTable, String> {
     @Override
     public List<ExampleTable> findAll() throws SQLException {
         List<ExampleTable> list = new ArrayList<>();
-        //language=PostgreSQL
         String query = "select id as id, name as name," +
                 "created_date as createdDate, " +
                 "created_time as createdTime, " +
@@ -104,7 +147,6 @@ public class ExampleTableDao implements CrudRepository<ExampleTable, String> {
 
     @Override
     public List<ExampleTable> findAll(Long start, Long limit, String orderDirection, ExampleTable param) throws SQLException {
-        //language=PostgreSQL
         String query = "select id           as id,\n" +
                 "       name         as name,\n" +
                 "       created_date as createdDate,\n" +
--- src/test/java/com/hanifiamdev/jdbc/ExampleTableTest.java
+++ src/test/java/com/hanifiamdev/jdbc/ExampleTableTest.java
@@ -8,8 +8,13 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import javax.sql.DataSource;
+import java.math.BigDecimal;
 import java.sql.Connection;
+import java.sql.Date;
 import java.sql.SQLException;
+import java.sql.Timestamp;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
 import java.util.List;
 import java.util.Optional;
 
@@ -41,7 +46,7 @@ public class ExampleTableTest {
     }
 
     @Test
-    public void testFindById() throws SQLException {
+    public void testFindById() {
         try (Connection connection = this.dataSource.getConnection()) {
             this.dao = new ExampleTableDao(connection);
 
@@ -51,7 +56,7 @@ public class ExampleTableTest {
             Assertions.assertEquals(id001.getName(), "Hanif Amrulalah");
 
             Optional<ExampleTable> notExistId = this.dao.findById("006");
-            Assertions.assertFalse( notExistId.isPresent(), "find by id 006 data not exist actualy");
+            Assertions.assertFalse(notExistId.isPresent(), "find by id 006 data not exist actualy");
         } catch (SQLException ex) {
             log.error("can't fetch data", ex);
         }
@@ -59,24 +64,92 @@ public class ExampleTableTest {
     }
 
     @Test
-    public void testPagination() throws SQLException {
+    public void testPagination() {
         try (Connection connection = this.dataSource.getConnection()) {
             this.dao = new ExampleTableDao(connection);
             ExampleTable params = new ExampleTable();
             params.setActive(true);
-            List<ExampleTable> datas = this.dao.findAll(0L,2L, "createdtime", params);
+            List<ExampleTable> datas = this.dao.findAll(0L, 2L, "createdtime", params);
             for (ExampleTable data : datas) {
-                log.info("Data Page Pertama : {} ",  data.toString());
+                log.info("Data Page Pertama : {} ", data.toString());
             }
             Assertions.assertEquals(2, datas.size());
-            List<ExampleTable> datas2 = this.dao.findAll(2L,3L, "createdtime", params);
+            List<ExampleTable> datas2 = this.dao.findAll(2L, 3L, "createdtime", params);
             for (ExampleTable data : datas2) {
-                log.info("Data Page Kedua : {} ",  data.toString());
+                log.info("Data Page Kedua : {} ", data.toString());
             }
             Assertions.assertEquals(3, datas2.size());
         } catch (SQLException ex) {
             log.error("can't fetch data", ex);
         }
 
+    }
+
+    @Test
+    void testUpdateData() {
+        try (Connection connection = this.dataSource.getConnection()) {
+            this.dao = new ExampleTableDao(connection);
+            ExampleTable params = new ExampleTable("003", "Pratama Arhan", Date.valueOf(LocalDate.now()),
+                    Timestamp.valueOf(LocalDateTime.now()), true, 0L, BigDecimal.ONE, null, 0f);
+            ExampleTable exampleTable = this.dao.update(params);
+            log.info(exampleTable.toString());
+            Assertions.assertEquals("Pratama Arhan", exampleTable.getName());
+
+        } catch (SQLException ex) {
+            log.error("can't update data", ex);
+        }
+    }
+
+    @Test
+    void testDeleteData() {
+        try (Connection connection = this.dataSource.getConnection()) {
+            this.dao = new ExampleTableDao(connection);
+            Boolean result = this.dao.removeById("005");
+            log.info("Deleted Success is {}", result);
+            Assertions.assertFalse(result, "Failed Deleted ");
+        } catch (SQLException ex) {
+            log.error("can't update data", ex);
+        }
+
+    }
+
+    @Test
+    void testSaveData() {
+        try (Connection connection = this.dataSource.getConnection()) {
+            this.dao = new ExampleTableDao(connection);
+            ExampleTable data = new ExampleTable(null, "Matesi Sulaiman", Date.valueOf(LocalDate.now()),
+                    Timestamp.valueOf(LocalDateTime.now()), true, 0L, BigDecimal.ONE, null, 0f);
+
+            data = this.dao.save(data);
+            Assertions.assertNotNull(data.getId(),"Example id save");
+            // select
+            Optional<ExampleTable>  optional = dao.findById(data.getId());
+            Assertions.assertTrue(optional.isPresent(), "Data telah disimpan");
+
+            // delete data
+            dao.removeById(data.getId());
+            optional = dao.findById(data.getId());
+            Assertions.assertFalse(optional.isPresent(), "Data telah dihapus");
+
+            // update
+            optional = dao.findById("003");
+            Assertions.assertTrue(optional.isPresent(), "find by id 003 is present");
+            data = optional.orElse(new ExampleTable());
+            String oldName = "Pratama Arhan";
+            Assertions.assertEquals( oldName, data.getName(), "Check Name For Id 003");
+
+            data.setName("Arho");
+            dao.update(data);
+
+            Optional<ExampleTable> optionalUpdated = dao.findById(data.getId());
+            ExampleTable newData =  optionalUpdated.orElse(new ExampleTable());
+            Assertions.assertEquals("Arho", newData.getName(), "isEqual For Name Updated");
+
+            // supaya balik nilai semula agar di running ulang tidak error
+            data.setName(oldName);
+            dao.update(data);
+        } catch (SQLException ex) {
+            log.error("can't update data", ex);
+        }
     }
 }
